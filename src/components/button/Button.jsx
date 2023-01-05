e8181db64169b49a79b17d8dd31d3febba579070
Leonel Velazquez <leonel.n.velazqu@gmail.com>
2023-01-05 16:47:41 -0300

    Tarjetas y navBar

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,5 +2,5 @@ import "./button.css"
 export default function Button(props){
 console.log(props);
 
-return <button className="btn">{props.text}</button>
+return <button className="boton">{props.text}</button>
 }
--- src/components/navegacion/navbar.js
+++ src/components/navegacion/navbar.js
@@ -1,28 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        <img src="./Favicon.png" alt = "logo" width="70px"></img>
-      </a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-          <a className="nav-link" href="#">Remeras</a>
-          <a className="nav-link" href="#">Tazas</a>
-          <a className="nav-link disabled">Disabled</a>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="./Favicon.png" alt="logo" width="70px"></img>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">
+                Inicio
+              </a>
+              <a className="nav-link" href="#">
+                Remeras
+              </a>
+              <a className="nav-link" href="#">
+                Tazas
+              </a>
+              <a className="nav-link disabled">Disabled</a>
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
-  </nav>
-  </div>
-  )
+  );
 }
 
 export default Navbar;
