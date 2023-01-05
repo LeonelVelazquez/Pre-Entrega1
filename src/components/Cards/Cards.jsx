3e339263d6744b348110855ea29d9f37fae88b9e
Leonel Velazquez <leonel.n.velazqu@gmail.com>
2023-01-05 13:53:30 -0300

    se modifica button y cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import Button from '../button/Button';
 function Cards(props) {
-  const {title, price, detail, imgurl} = props;
-  return (
-    <div>
-        <img width="200px" src={imgurl} alt="Imagen" />
-        <center>
-          <div>
-            <h3>{title}</h3>
-            <h4>{price}</h4>
-            <p>{detail}</p>
-        </div>
-        <Button>Ver detalles</Button>
-        </center>
-        
-    </div>
-    
-  )
+const {title, price, detail, imgurl} = props;
+return (
+<div>
+<img width="200px" src={imgurl} alt="Imagen" />
+<center>
+<div>
+<h3>{title}</h3>
+<h4>{price}</h4>
+<p>{detail}</p>
+</div>
+<Button text={'Ver detalles'}/>
+</center>
+
+</div>
+
+)
 }
 
 export default Cards
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,6 +1,6 @@
 import "./button.css"
 export default function Button(props){
-    console.log(props);
+console.log(props);
 
-    return <button className="btn">{props.text}</button>
+return <button className="btn">{props.text}</button>
 }
